027844b4016befb9643f7ed6e430e7d055c46306
Andre Beolchi <andregarbini2001@gmail.com>
2024-05-13 22:40:37 -0300

    fix alignment

--- src/views/ProjectsSection.tsx
+++ src/views/ProjectsSection.tsx
@@ -55,7 +55,7 @@ export default function ProjectsSection() {
 												className="flex items-center gap-x-4 text-gray-600 mt-6"
 											>
 												<ChatBubbleBottomCenterTextIcon
-													className="h-6 w-6 leading-8"
+													className={`h-6 ${isEven ? "w-6" : "w-[48px]"} leading-8`}
 												/>
 												<p className="text-lg leading-8">{item?.description}</p>
 											</div>
